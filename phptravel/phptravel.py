ebb3285416866aaa8524e49bea234587f2fb57d4
Sergiu ANDRONE <sandrone@bitdefender.com>
2020-02-26 21:48:26 +0200

    Update phptravel.py
    splitting page objects into multiple objects
    added testcases

--- phptravel/phptravel.py
+++ phptravel/phptravel.py
@@ -1,23 +1,149 @@
 from selenium import webdriver
 import getpass
 import time
+import sys
+
+
+class Tests:
+    def __init__(self):
+        self.logger = Logger()
+        self.session = PhpTravel()
+        self.page_loaded = True
+        self.page_objects = PageObjects()
+
+    def return_home(self):
+        main_page = self.session.find_element_by_xpath(xpath=self.page_objects.main_page)
+        if main_page:
+            main_page.click()
+
+    def test00(self):
+        time_loaded = self.session.open_page()
+        time.sleep(5)
+
+        if self.page_objects.page_title in self.session.driver.title:
+            self.logger.log_data(f"Page LOADED in {time_loaded} seconds \n\n")
+        else:
+            self.logger.log_data(f"Page FAILED to load. Exiting tests...\n")
+            self.page_loaded = False
+
+    def test01(self):
+        self.logger.log_data("TEST 01 >> Checking if the following buttons exists, are clickable and become active: Flights, Tours, Cars, Visa, Hotels\n")
+
+        buttons = {"Flights": self.page_objects.flights_menu.flights_button, "Tours": self.page_objects.tours_menu.tours_button, "Cars": self.page_objects.cars_menu.cars_button,
+                   "Visa": self.page_objects.visa_menu.visa_button, "Hotels": self.page_objects.hotels_menu.hotels_button}
+
+        for key in buttons.keys():
+            result = self.session.find_element_by_xpath(xpath=buttons[key])
+            if result:
+                result.click()
+                time.sleep(1)
+                if result.is_enabled():
+                    self.logger.log_data(f"{result.text}  >>  PASS")
+                else:
+                    self.logger.log_data(f"{result.text}  >>  FAIL: button present but inactive")
+            else:
+                self.logger.log_data(f"{key}  >>  FAIL: button not present")
+        self.logger.log_data("\n\n")
+        self.return_home()
+
+    def test02(self):
+        self.logger.log_data("TEST 02 >> checking the labels in: Hotels\n")
+
+        expected_labels = {self.page_objects.hotels_menu.destination_text: self.page_objects.hotels_menu.destination_label,
+                           self.page_objects.hotels_menu.checkin_text: self.page_objects.hotels_menu.checkin_label,
+                           self.page_objects.hotels_menu.checkout_text: self.page_objects.hotels_menu.checkout_label,
+                           self.page_objects.hotels_menu.adults_text: self.page_objects.hotels_menu.adults_label,
+                           self.page_objects.hotels_menu.child_text: self.page_objects.hotels_menu.child_label}
+        found_labels = []
+
+        hotels = self.session.find_element_by_xpath(xpath=self.page_objects.hotels_menu.hotels_button)
+        if hotels:
+            hotels.click()
+            time.sleep(1)
+            if hotels.is_enabled():
+                for key in expected_labels.keys():
+                    element = self.session.find_element_by_xpath(xpath=expected_labels[key])
+                    if element:
+                        found_labels.append(element.text)
+                for label in expected_labels.keys():
+                    if label in found_labels:
+                        self.logger.log_data(f"{label} is present in the Hotels menu  >>  PASS")
+                    else:
+                        self.logger.log_data(f"{label} is not present in the Hotels menu  >>  FAIL")
+            else:
+                self.logger.log_data("'Hotels' menu is not active  >>  FAIL")
+        else:
+            self.logger.log_data("'Hotels' menu wasn't found  >>  FAIL")
+
+        self.logger.log_data("\n\n")
+        self.return_home()
+
+    def test03(self):
+        self.logger.log_data("TEST 03 >> checking the labels in: Flights\n")
+
+        expected_labels = {self.page_objects.flights_menu.from_text: self.page_objects.flights_menu.from_label,
+                           self.page_objects.flights_menu.to_text: self.page_objects.flights_menu.to_label,
+                           self.page_objects.flights_menu.depart_text: self.page_objects.flights_menu.depart_label,
+                           self.page_objects.flights_menu.adults_text: self.page_objects.flights_menu.adults_label,
+                           self.page_objects.flights_menu.child_text: self.page_objects.flights_menu.child_label,
+                           self.page_objects.flights_menu.infant_text: self.page_objects.flights_menu.infant_label}
+        found_labels = []
+
+        flights = self.session.find_element_by_xpath(xpath=self.page_objects.flights_menu.flights_button)
+        if flights:
+            flights.click()
+            time.sleep(1)
+            if flights.is_enabled():
+                for key in expected_labels.keys():
+                    element = self.session.find_element_by_xpath(xpath=expected_labels[key])
+                    if element:
+                        found_labels.append(element.text)
+                for label in expected_labels.keys():
+                    if label in found_labels:
+                        self.logger.log_data(f"{label} is present in the Hotels menu  >>  PASS")
+                    else:
+                        self.logger.log_data(f"{label} is not present in the Hotels menu  >>  FAIL")
+            else:
+                self.logger.log_data("'Flights' menu is not active  >>  FAIL")
+        else:
+            self.logger.log_data("'Flights' menu wasn't found  >>  FAIL")
+
+        self.logger.log_data("\n\n")
+        self.return_home()
+
+    def test04(self):
+        field_not_selected = self.session.find_element_by_xpath(self.page_objects.hotels_menu.destination_field_inactive)
+        if field_not_selected:
+            field_not_selected.click()
+        field_selected = self.session.find_element_by_xpath(self.page_objects.hotels_menu.destination_field_active)
+        if field_selected:
+            field_selected.send_keys("Berlin")
+        
+        search = self.session.find_element_by_xpath(xpath=self.page_objects.hotels_menu.search_button)
+        if search:
+            search.submit()
+        time.sleep(5)
+        self.return_home()
+
+    def close_test(self):
+        self.session.close_session()
 
 
 class Logger:
     def __init__(self):
+        self.log_location = f"/Users/{getpass.getuser()}/Desktop/test_results.txt" if sys.platform == "darwin" else "C:\\"
+
         self.create_log_file()
 
-    @staticmethod
-    def create_log_file():
+    def create_log_file(self):
         try:
-            with open(f'/Users/{getpass.getuser()}/Desktop/test_results.txt', 'w') as f:
+            with open(self.log_location, 'w') as f:
                 f.write('THE RESULTS OF THE TEST ARE: \n\n\n')
         except Exception as e:
             print(f"Results file couldn't be created. The test result won't be recoreded. Error: {e}")
 
-    @staticmethod
-    def log_data(text):
-        with open(f'/Users/{getpass.getuser()}/Desktop/test_results.txt', 'a') as f:
+    def log_data(self, text):
+        with open(self.log_location, 'a') as f:
             f.write(text + '\n')
 
 
@@ -74,132 +200,93 @@ class PhpTravel:
 
 
 class PageObjects:
+    """
+
+    All the page elements used in tests must be retrieved from here. Can be created private and returned as @property
+
+    """
     def __init__(self):
         self.page_title = "PHPTRAVELS"
 
-        # elements represented by xpath (can also be created private and returned as @property)
-        self.hotels_button = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/nav/ul/li[1]/a"
-        self.flights_button = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/nav/ul/li[2]/a"
-        self.tours_button = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/nav/ul/li[3]/a"
-        self.cars_button = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/nav/ul/li[4]/a"
-        self.visa_button = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/nav/ul/li[5]/a"
+        # xpath for main page
+        self.main_page = "/html//header[@id='header-waypoint-sticky']/div[@class='header-top']//a[@href='https://www.phptravels.net/']/img[@alt='PHPTRAVELS | Travel Technology Partner']"
 
-        self.hotels_destination_text = "DESTINATION"
-        self.hotels_checkin_text = "CHECK IN"
-        self.hotels_checkout_text = "CHECK OUT"
-        self.hotels_adults_text = "ADULTS (12-75)"
-        self.hotels_child_text = "CHILD (2-12)"
-
-        self.hotels_destination_label = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[1]/div/div/form/div/div/div[1]/div/label"
-        self.hotels_checkin_label = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[1]/div/div/form/div/div/div[2]/div/div/div[1]/div/label"
-        self.hotels_checkout_label = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[1]/div/div/form/div/div/div[2]/div/div/div[2]/div/label"
-        self.hotels_adults_label = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[1]/div/div/form/div/div/div[3]/div/div/div/div/div/div/div[1]/div/label"
-        self.hotels_child_label = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[1]/div/div/form/div/div/div[3]/div/div/div/div/div/div/div[2]/div/label"
+        self.hotels_menu = HotelsMenu()
+        self.flights_menu = FlightsMenu()
+        self.tours_menu = ToursMenu()
+        self.cars_menu = CarsMenu()
+        self.visa_menu = VisaMenu()
 
 
-class Tests:
+class VisaMenu:
     def __init__(self):
-        self.logger = Logger()
-        self.session = PhpTravel()
-        self.page_loaded = True
-        self.page_objects = PageObjects()
+        # buttons/labels represented by xpath
+        self.visa_button = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/nav/ul/li[5]/a"
+        self.visa_submit_button = ""
 
-    def test00(self):
-        time_loaded = self.session.open_page()
-        time.sleep(5)
 
-        if self.page_objects.page_title in self.session.driver.title:
-            self.logger.log_data(f"Page LOADED in {time_loaded} seconds \n\n")
-        else:
-            self.logger.log_data(f"Page FAILED to load. Exiting tests...\n")
-            self.page_loaded = False
+class CarsMenu:
+    def __init__(self):
+        # buttons/labels represented by xpath
+        self.cars_button = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/nav/ul/li[4]/a"
+        self.search_button = ""
 
-    def test01(self):
-        self.logger.log_data("TEST 01 >> checking the following buttons: Flights, Tours, Cars, Visa, Hotels\n")
 
-        buttons = {"Flights": self.page_objects.flights_button, "Tours": self.page_objects.tours_button, "Cars": self.page_objects.cars_button, "Visa": self.page_objects.visa_button,
-                   "Hotels": self.page_objects.hotels_button}
+class ToursMenu:
+    def __init__(self):
+        # buttons/labels represented by xpath
+        self.tours_button = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/nav/ul/li[3]/a"
+        self.search_button = ""
 
-        for key in buttons.keys():
-            result = self.session.find_element_by_xpath(xpath=buttons[key])
-            if result:
-                result.click()
-                time.sleep(1)
-                if result.is_enabled():
-                    self.logger.log_data(f"{result.text}  >>  PASS")
-                else:
-                    self.logger.log_data(f"{result.text}  >>  FAIL: button present but inactive")
-            else:
-                self.logger.log_data(f"{key}  >>  FAIL: button not present")
-        self.logger.log_data("\n\n")
 
-    def test02(self):
-        self.logger.log_data("TEST 02 >> checking menu: Hotels\n")
+class FlightsMenu:
+    def __init__(self):
+        # buttons/labels represented by xpath
+        self.flights_button = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/nav/ul/li[2]/a"
+        self.search_button = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[2]/div/div/form/div/div[3]/div[4]/button"
 
-        expected_labels = {self.page_objects.hotels_destination_text: self.page_objects.hotels_destination_label,
-                           self.page_objects.hotels_checkin_text: self.page_objects.hotels_checkin_label,
-                           self.page_objects.hotels_checkout_text: self.page_objects.hotels_checkout_label,
-                           self.page_objects.hotels_adults_text: self.page_objects.hotels_adults_label,
-                           self.page_objects.hotels_child_text: self.page_objects.hotels_child_label}
-        found_labels = []
+        # name of the labels in "Flights" submenu
+        self.from_text = "FROM"
+        self.to_text = "TO"
+        self.depart_text = "DEPART"
+        self.adults_text = "ADULTS"
+        self.child_text = "CHILD"
+        self.infant_text = "INFANT"
 
-        hotels = self.session.find_element_by_xpath(xpath=self.page_objects.hotels_button)
-        if hotels:
-            hotels.click()
-            time.sleep(1)
-            if hotels.is_enabled():
-                for key in expected_labels.keys():
-                    element = self.session.find_element_by_xpath(xpath=expected_labels[key])
-                    found_labels.append(element.text)
-                for label in expected_labels.keys():
-                    if label in found_labels:
-                        self.logger.log_data(f"{label} is present in the Hotels menu  >>  PASS")
-                    else:
-                        self.logger.log_data(f"{label} is not present in the Hotels menu  >>  FAIL")
-            else:
-                self.logger.log_data("'Hotels' menu is not active  >>  FAIL")
-        else:
-            self.logger.log_data("'Hotels' menu wasn't found  >>  FAIL")
+        # xpath of the labels in "Flights" submenu
+        self.from_label = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[2]/div/div/form/div/div[3]/div[1]/div/div[1]/div/label"
+        self.to_label = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[2]/div/div/form/div/div[3]/div[1]/div/div[2]/div/label"
+        self.depart_label = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[2]/div/div/form/div/div[3]/div[2]/div/div/div[1]/div/label"
+        self.adults_label = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[2]/div/div/form/div/div[3]/div[3]/div/div/div[1]/div/label"
+        self.child_label = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[2]/div/div/form/div/div[3]/div[3]/div/div/div[2]/div/label"
+        self.infant_label = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[2]/div/div/form/div/div[3]/div[3]/div/div/div[3]/div/label"
 
-        self.logger.log_data("\n\n")
 
-    def test03(self):
-        self.logger.log_data("TEST 03 >> checking menu: Flights\n")
-        expected_labels01 = {"FROM": "col-md-6.col-6", "TO": "col-md-6.col-6", "DEPART": "class12.col-6.col-12"}
-        expected_labels02 = {"ADULTS": "col-md-3.col-xs-12", "CHILD": "col-md-3.col-xs-12", "INFANT": "col-md-3.col-xs-12"}
-        found_labels = []
+class HotelsMenu:
+    def __init__(self):
+        # buttons/labels represented by xpath
+        self.hotels_button = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/nav/ul/li[1]/a"
+        self.search_button = "/html//div[@id='hotels']/div//form[@role='search']//button[@type='submit']"
 
-        flights = self.session.find_element_by_link("Flights")
-        if flights:
-            flights.click()
-            time.sleep(1)
-            if flights.is_enabled():
-                for value in expected_labels01.values():
-                    labels = self.session.find_elements_by_class(cls=value)
-                    for element in labels:
-                        found_labels.append(element.find_element_by_class_name("form-group").find_element_by_tag_name("label").text)
-                for label in expected_labels01.keys():
-                    if label in found_labels:
-                        self.logger.log_data(f"{label} is present in the Hotels menu  >>  PASS")
-                    else:
-                        self.logger.log_data(f"{label} is not present in the Hotels menu  >>  FAIL")
-                found_labels.clear()
-                for value in expected_labels02.values():
-                    labels = self.session.find_elements_by_class(cls=value)
-                    for element in labels:
-                        if element.find_elements_by_class_name("col-4"):
-                            for item in element.find_elements_by_class_name("col-4"):
-                                if item.find_element_by_class_name("form-group.form-spin-group").find_element_by_tag_name("label"):
-                                    found_labels.append(item.find_element_by_class_name("form-group.form-spin-group").find_element_by_tag_name("label").text)
-                for label in expected_labels02.keys():
-                    if label in found_labels:
-                        self.logger.log_data(f"{label} is present in the Hotels menu  >>  PASS")
-                    else:
-                        self.logger.log_data(f"{label} is not present in the Hotels menu  >>  FAIL")
-        self.logger.log_data("\n\n")
+        # name of the labels in "Hotels" submenu
+        self.destination_text = "DESTINATION"
+        self.checkin_text = "CHECK IN"
+        self.checkout_text = "CHECK OUT"
+        self.adults_text = "ADULTS (12-75)"
+        self.child_text = "CHILD (2-12)"
 
-    def close_test(self):
-        self.session.close_session()
+        # xpath of the labels in "Hotels" submenu
+        self.destination_label = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[1]/div/div/form/div/div/div[1]/div/label"
+        self.checkin_label = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[1]/div/div/form/div/div/div[2]/div/div/div[1]/div/label"
+        self.checkout_label = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[1]/div/div/form/div/div/div[2]/div/div/div[2]/div/label"
+        self.adults_label = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[1]/div/div/form/div/div/div[3]/div/div/div/div/div/div/div[1]/div/label"
+        self.child_label = "/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[1]/div/div/form/div/div/div[3]/div/div/div/div/div/div/div[2]/div/label"
+
+        # xpath for editable fields
+        self.destination_field_inactive = "/html//div[@id='hotels']/div//form[@role='search']//div[@class='form-icon-left typeahead__container']/div/a[@href='javascript:void(0)']/span[@class='select2-chosen']"
+        self.destination_field_active = "//div[@id='select2-drop']//input[@type='text']"
+
+
 
 
 def main():
@@ -209,9 +296,15 @@ def main():
 
     if tests.page_loaded:
         tests.test01()
+        time.sleep(3)
         tests.test02()
-        #tests.test03()
-        tests.close_test()
+        time.sleep(3)
+        tests.test03()
+        time.sleep(3)
+        tests.test04()
+        time.sleep(3)
+
+    tests.close_test()
 
 
 if __name__ == '__main__':
